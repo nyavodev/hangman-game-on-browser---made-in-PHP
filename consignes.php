968c888a65805b3870ae35401a186bf9599dafd0
NyAvo Dev <59570964+nyavodev@users.noreply.github.com>
2020-04-09 22:01:51 +0300

    Add files via upload

---
+++ consignes.php
@@ -0,0 +1,8 @@
+<div class="col-6 pr-5">
+	<p class="h3 d-flex justify-content-center text-danger my-3">ICI LES CONSIGNES DU JEU</p>
+	<P>
+		Le but du jeu est de trouver un mot en tapant une lettre à chaque fois. Si la lettre est dans le mot secret, alors les lettres trouvées dans le mot se dévoilent. <br/>
+		Comme indice, vous aurez le nombre de caractères du mot secret dès le début d'une partie. <br/>
+		<strong class="text-info d-inline-block my-3"><span class="text-warning">REMARQUE :</span> Le mot secret est généré au hazard parmi ceux définis.</strong>
+	</P>
+</div>
---
+++ form.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+	<div class="form-group">
+		<label for="lettre" title="Puis validez biensûr ! (IMBECILE :p :D)">Entrez une lettre ici :</label>
+		<div class="input-group">	
+			<input type="text" name="lettre" placeholder="a-z | A-Z" id="lettre" class="form-control" autofocus>
+			<input type="submit" value="Valider la lettre entrée" class="btn btn-primary form-control">
+		</div>
+	</div>
+</form>
